6a07ad192db770c8d538258fed3d3e6290ee0eb5
Mateusz <58079969+worscik@users.noreply.github.com>
2024-07-22 21:45:23 +0200

    created project
    created project

---
+++ src/main/java/pl/financemanagement/FinancemanagementApplication.java
@@ -0,0 +1,13 @@
+package pl.financemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancemanagementApplication.class, args);
+	}
+
+}
