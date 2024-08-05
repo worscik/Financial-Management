052743cb5c431e42aff2a7b3727faaf348f8778f
mkaczmarski <mateusz.kaczmarski@salesmanago.com>
2024-08-05 12:19:14 +0200

    Fix Qualifier

--- src/main/java/pl/financemanagement/User/UserService/UserServiceImpl.java
+++ src/main/java/pl/financemanagement/User/UserService/UserServiceImpl.java
@@ -3,25 +3,15 @@ package pl.financemanagement.User.UserService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import pl.financemanagement.BankAccount.Repository.UsersRepository;
-import pl.financemanagement.User.UserModel.UserDto;
 import pl.financemanagement.User.UserModel.UserRequest;
 import pl.financemanagement.User.UserModel.UserResponse;
-
-import java.util.Optional;
 
 @Service
 @Qualifier("normalUserService")
 public class UserServiceImpl implements UserService {
 
-    private final UsersRepository usersRepository;
-
-    public UserServiceImpl(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
-
     @Override
     public UserResponse createUser(UserRequest userRequest) {
-        usersRepository.save()
         return new UserResponse(false);
     }
 
