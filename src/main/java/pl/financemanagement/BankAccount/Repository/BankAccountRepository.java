68bb44e452a02776ec104cf4cfb1af6a667f4cdb
mateusz.kaczmarski <kaczmarskim95@gmail.com>
2025-01-14 11:54:29 +0100

    Change UserRepository to JPA

---
+++ src/main/java/pl/financemanagement/BankAccount/Repository/BankAccountRepository.java
@@ -0,0 +1,22 @@
+package pl.financemanagement.BankAccount.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pl.financemanagement.BankAccount.Model.BankAccount;
+
+import java.util.Optional;
+
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+
+    @Query(nativeQuery = true, value = """
+                        SELECT * 
+                        FROM bank_account  
+                        WHERE  id = :id
+            """)
+    Optional<BankAccount> findBankAccountById(@Param("id") long id);
+
+
+}
