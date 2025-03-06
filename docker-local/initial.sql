5e7c6b722d58996fa118d21b9e7a7c967a494ece
mateusz.kaczmarski <kaczmarskim95@gmail.com>
2025-03-06 19:18:08 +0100

    Fix initial queries

--- docker-local/initial.sql
+++ docker-local/initial.sql
@@ -17,9 +17,10 @@ CREATE TABLE bank_account (
                               created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                               modify_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                               account_version BIGINT NOT NULL,
-                              account_name VARCHAR(255),
-                              account_number VARCHAR(255),
+                              account_name VARCHAR(255) NOT NULL,
+                              account_number CHAR(36) NOT NULL,
                               account_balance DECIMAL(19, 4),
+                              currency VARCHAR(255),
                               FOREIGN KEY (user_id) REFERENCES user_account(id)
 );
 
@@ -41,13 +42,13 @@ INSERT INTO user_account (external_id, email, name, version, user_role, password
 VALUES
     (UUID(), 'admin@financialapp.com', 'admin', 1, 'ADMIN', '$2a$12$YsXuaGHGUoXSdwsMtzenY.p2A5bB.chYlBYcdCQ..QWXlKOOOMqHG'),
     (UUID(), 'demo@financialapp.com', 'demo', 1, 'USER', '$2a$12$kIB0Vsuy9Gqm/jz.LJdZPu4Nzh9Gn33CsvQ.D510wnH9.Zg4uHNzq'),
-    (UUID(), 'user@financialapp.com', 'user', 1, 'USER', '$2a$12$8tokTl6alcZURDFAWSqZ4et6eSL6jy33RLpEH8y470sN7CGWYNg/.');
+    (UUID(), 'user@financialapp.com', 'user', 1, 'USER', '$2a$12$8tokTl6alcZURDFAWSqZ4et6eSL6jy33RLpEH8y470sN7CGWYNg/');
 
-INSERT INTO bank_account (external_id, user_id, account_version, account_name, account_number, account_balance)
+INSERT INTO bank_account (external_id, user_id, account_version, account_name, account_number, account_balance, currency)
 VALUES
-    (UUID(), (SELECT id FROM user_account WHERE email = 'admin@financialapp.com'), 1, 'Admin Account', '1234567890', 1000.00),
-    (UUID(), (SELECT id FROM user_account WHERE email = 'demo@financialapp.com'), 1, 'Demo Bank Account', '0987654321', 500.00),
-    (UUID(), (SELECT id FROM user_account WHERE email = 'user@financialapp.com'), 1, 'User Bank Account', '0987654331', 500.00);
+    (UUID(), (SELECT id FROM user_account WHERE email = 'admin@financialapp.com'), 1, 'Admin Account', '9e1f957c-3239-43a4-bd36-550e2ebc03ac', 1000.00, 'USD'),
+    (UUID(), (SELECT id FROM user_account WHERE email = 'demo@financialapp.com'), 1, 'Demo Bank Account', 'ea8ff5cf-6b4e-4e1f-8629-a6806da85f55', 500.00, 'USD'),
+    (UUID(), (SELECT id FROM user_account WHERE email = 'user@financialapp.com'), 1, 'User Bank Account', '14d67eea-a317-4b6e-88e7-bc5da229f280', 500.00, 'USD');
 
 INSERT INTO expense (external_id, version, created_on, modify_on, expense_item, expense_category, expense_type, expense, user_id)
 VALUES
